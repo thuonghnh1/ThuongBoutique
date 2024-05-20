a07c2909955cf45611b58c62f35553daab5dd8ee
Thuong <Thuong@DESKTOP-QOEV1DB>
2024-05-20 11:34:49 +0700

    cấu trú dự án

---
+++ src/main/java/edu/poly/shop/AssignmnetApplication.java
@@ -0,0 +1,13 @@
+package edu.poly.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmnetApplication.class, args);
+	}
+
+}
