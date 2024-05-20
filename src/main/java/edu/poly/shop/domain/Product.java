c5bc8e9a270d08a2378884a584f49629fc34d728
Thuong <Thuong@DESKTOP-QOEV1DB>
2024-05-20 11:51:39 +0700

    commit domain

---
+++ src/main/java/edu/poly/shop/domain/Product.java
@@ -0,0 +1,22 @@
+package edu.poly.shop.domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	private int productId;
+	private String name;
+	private int quantity;
+	private double unitPrice;
+	private String image;
+	private String description;
+	private double discount;
+	private Date entereDate;
+	private short status;
+	private int categoryId;
+}
